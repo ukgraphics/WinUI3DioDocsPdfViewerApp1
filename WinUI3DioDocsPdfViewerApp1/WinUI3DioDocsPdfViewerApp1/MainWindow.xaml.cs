94191b699e7f3f614095b5bc6b39b2240625cb18
Kuni <kuni.ujiie@grapecity.com>
2021-07-07 14:13:50 +0900

    プロジェクト ファイルを追加します。

---
+++ WinUI3DioDocsPdfViewerApp1/WinUI3DioDocsPdfViewerApp1/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WinUI3DioDocsPdfViewerApp1
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            //MyWebView.Source = new Uri("ms-appx://wwwroot/index.html");
+
+            MyWebView.Source = new Uri("file:///C:/Users/kuni/Desktop/WinUI3DioDocsPdfViewerApp1/WinUI3DioDocsPdfViewerApp1/WinUI3DioDocsPdfViewerApp1/wwwroot/index.html");
+
+        }
+
+        private void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Uri targetUri = new Uri(addressBar.Text);
+                MyWebView.Source = targetUri;
+            }
+            catch (FormatException ex)
+            {
+                // Incorrect address entered.
+            }
+        }
+    }
+}
